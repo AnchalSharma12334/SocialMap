d82654fb770f461753d54fb0c6301445ea3090fa
Anchal Sharma <AnchalSharma12334>
2025-05-16 02:09:47 +0530

    Added Auth

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider, TwitterAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: 'AIzaSyDsXlldaY37KijgZc8jE-oi1LxUAQChx2Y',
+  authDomain: 'socialmap-a3b94.firebaseapp.com"',
+  projectId: 'socialmap-a3b94',
+  storageBucket: 'socialmap-a3b94.firebasestorage.app',
+  messagingSenderId: '159825649826',
+  appId: '1:159825649826:web:7282da4a88a35e46dfb7fb',
 };
 
 const app = initializeApp(firebaseConfig);
